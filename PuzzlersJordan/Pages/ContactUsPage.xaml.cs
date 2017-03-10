aae49292a8fdc5077ad2e7119f6a065c0fc37852
Amjad Alhusseini <amjad.alhusseini@aramex.com>
2017-03-10 19:34:51 +0200

    Added a new page - contact us with all it's contents

--- PuzzlersJordan/Pages/ContactUsPage.xaml.cs
+++ PuzzlersJordan/Pages/ContactUsPage.xaml.cs
@@ -11,5 +11,22 @@ namespace PuzzlersJordan
 		{
 			InitializeComponent();
 		}
+
+		public void facebook_clicked(object sender, EventArgs e)
+		{
+			Device.OpenUri(new Uri("https://www.facebook.com/PuzzlersJordan"));
+		}
+		public void youtube_clicked(object sender, EventArgs e)
+		{
+			Device.OpenUri(new Uri("https://www.youtube.com/user/puzzlersjo"));
+		}
+		public void twitter_clicked(object sender, EventArgs e)
+		{
+			Device.OpenUri(new Uri("https://twitter.com/Puzzlersjordan"));
+		}
+		public void phone_clicked(object sender, EventArgs e)
+		{
+			Device.OpenUri(new Uri("tel:+962799892728"));
+		}
 	}
 }
